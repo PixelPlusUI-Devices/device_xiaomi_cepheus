9d776e08c46e535b8e447cfe8636a1a9fbd7cc22
Nauval Rizky <enuma.alrizky@gmail.com>
2020-01-31 11:57:48 +0900

    cepheus: parts: Introduce raise to wake gesture
    * Highly inspired by iOS raise to wake
    * Add min interval for raise2wake
    * Check proximity before wakeup
    
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: David Setiawan <fryevia@foxmail.com>

--- parts/src/org/lineageos/settings/doze/PickupSensor.java
+++ parts/src/org/lineageos/settings/doze/PickupSensor.java
@@ -25,6 +25,8 @@ import android.hardware.SensorManager;
 import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
 import android.os.SystemClock;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 import android.util.Log;
 
 import java.util.concurrent.ExecutorService;
@@ -36,6 +38,7 @@ public class PickupSensor implements SensorEventListener {
     private static final String TAG = "PickupSensor";
 
     private static final int MIN_PULSE_INTERVAL_MS = 2500;
+    private static final int MIN_WAKEUP_INTERVAL_MS = 1000;
     private static final int WAKELOCK_TIMEOUT_MS = 300;
 
     private SensorManager mSensorManager;
@@ -50,7 +53,7 @@ public class PickupSensor implements SensorEventListener {
     public PickupSensor(Context context) {
         mContext = context;
         mSensorManager = mContext.getSystemService(SensorManager.class);
-        mSensor = DozeUtils.getSensor(mSensorManager, "xiaomi.sensor.pickup");
+        mSensor = SensorsUtils.getSensor(mSensorManager, "xiaomi.sensor.pickup");
         mPowerManager = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
         mWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
         mExecutorService = Executors.newSingleThreadExecutor();
@@ -62,11 +65,11 @@ public class PickupSensor implements SensorEventListener {
     public void onSensorChanged(SensorEvent event) {
         boolean isRaiseToWake = DozeUtils.isRaiseToWakeEnabled(mContext);
 
-        if (DEBUG)
-            Log.d(TAG, "Got sensor event: " + event.values[0]);
+
+        if (DEBUG) Log.d(TAG, "Got sensor event: " + event.values[0]);
 
         long delta = SystemClock.elapsedRealtime() - mEntryTimestamp;
-        if (delta < MIN_PULSE_INTERVAL_MS) {
+        if (delta < (isRaiseToWake ? MIN_WAKEUP_INTERVAL_MS : MIN_PULSE_INTERVAL_MS)) {
             return;
         }
 
